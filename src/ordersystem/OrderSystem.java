7c72889b0e6c2c07f42f2076d829a40052c8885f
JoshGrylls <42368804+JoshGrylls@users.noreply.github.com>
2018-10-04 22:07:59 +1000

    Added textual command based interface

--- src/ordersystem/OrderSystem.java
+++ src/ordersystem/OrderSystem.java
@@ -1,5 +1,6 @@
 package ordersystem;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.Scanner;
 
 public class OrderSystem {
 
@@ -50,7 +51,7 @@ public class OrderSystem {
             return totalPrice;
         }
         
-        public float getTime() {
+        public int getTime() {
             for (Product item : items) {
                 totalPrep += item.prepTime;
             }
@@ -66,17 +67,77 @@ public class OrderSystem {
     
     
     public static void main(String[] args) {
+        
+        ArrayList<Order> orderList = new ArrayList<>();
+        ArrayList<Product> allProducts = new ArrayList<>();
+        
         Product item1 = new Product("item1", (float)10.5, "Item 1 description", 5);
+        allProducts.add(item1);
         Product item2 = new Product("item2", (float)9.0, "Item 2 description", 4);
+        allProducts.add(item2);
         Product item3 = new Product("item3", (float)5.5, "Item 3 description", 2);
+        allProducts.add(item3);
         Product item4 = new Product("item4", (float)2.5, "Item 4 description", 1);
+        allProducts.add(item4);
         
-        //List<Orders> orderList = new ArrayList<Orders>;   
+        Scanner scan = new Scanner(System.in);
         
-        Order order1 = new Order(1);
-        order1.addToOrder(item1, 1);
-        System.out.print("Price: " + order1.getPrice());
-        System.out.print(" || Time: " + order1.getTime());
-    }
-    
+        int decider = 0;
+        int orderId = 1;
+        while(decider != 3){
+            System.out.print("1. New Order \n2. New Product \n3. End Session \n");
+            decider = scan.nextInt();
+            
+            if(decider == 1){
+                Order newOrder = new Order(orderId);
+                orderId++;
+                
+                int deciderO = 0;
+                while(deciderO != 2) {
+                    System.out.print("1. Add Item \n2. Finish Order \n");
+                    deciderO = scan.nextInt();
+                    
+                    if (deciderO == 1) {
+                        int i = 1;
+                        for(Product item : allProducts) {
+                            System.out.print(i+". "+item.name+"\n");
+                            i++;
+                        }
+                        int nextItem = scan.nextInt();
+                        
+                        System.out.print("Enter the Quantity: ");
+                        int quantity = scan.nextInt();
+                        
+                        switch(nextItem) {
+                            case 1: newOrder.addToOrder(item1, quantity);
+                                    break;
+                            case 2: newOrder.addToOrder(item2, quantity);
+                                    break;
+                            case 3: newOrder.addToOrder(item3, quantity);
+                                    break;
+                            case 4: newOrder.addToOrder(item4, quantity);
+                                    break;
+                        }
+                    }
+                }
+                orderList.add(newOrder);
+                System.out.print("Order: " + newOrder.id);
+                System.out.print(" || Price: " + newOrder.getPrice());
+                System.out.print(" || ETA: " + newOrder.getTime() + "\n");
+            }
+            if(decider == 2){
+                System.out.print("Enter the product name: ");
+                String newName = scan.next();
+                System.out.print("Enter the product price: ");
+                float newPrice = scan.nextFloat();
+                System.out.print("Enter the product description: ");
+                String newDesc = scan.next();
+                System.out.print("Enter the product preparation time: ");
+                int newPrep = scan.nextInt();
+                Product newProduct = new Product(newName, newPrice, newDesc, newPrep);
+                allProducts.add(newProduct);
+                System.out.print("Product added \n");
+            }
+        } 
+    } 
 }
