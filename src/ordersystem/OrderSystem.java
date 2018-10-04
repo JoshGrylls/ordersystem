e720d64389d3e01d7fe5d6ce9b65d49f624e1fdd
JoshGrylls <42368804+JoshGrylls@users.noreply.github.com>
2018-10-04 22:19:36 +1000

    added comments to code

--- src/ordersystem/OrderSystem.java
+++ src/ordersystem/OrderSystem.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class OrderSystem {
 
-    public static class Product {
+    public static class Product { // base product class
         public String name;
         public float price;
         public String description;
@@ -17,26 +17,26 @@ public class OrderSystem {
             prepTime = time;
         }
         
-        public void editName(String newName) {
+        public void editName(String newName) { //change name
             name = newName;
         }
         
-        public void editPrice(float newPrice) {
+        public void editPrice(float newPrice) { //change price
             price = newPrice;
         }
         
-        public void editDesc(String newDesc) {
+        public void editDesc(String newDesc) { //change description
             description = newDesc;
         }
         
-        public void editTime(int newTime) {
+        public void editTime(int newTime) { //change preparation time
             prepTime = newTime;
         }
     }
     
-    public static class Order {
-        public ArrayList<Product> items = new ArrayList<>();
-        public int id;
+    public static class Order { //base order class
+        private ArrayList<Product> items = new ArrayList<>();
+        private int id;
         public float totalPrice;
         public int totalPrep;
         
@@ -44,21 +44,21 @@ public class OrderSystem {
             id = newId;
         }
         
-        public float getPrice() {
+        public float getPrice() { //calculate price
             for (Product item : items) {
                 totalPrice += item.price;
             }
             return totalPrice;
         }
         
-        public int getTime() {
+        public int getTime() { //calculate preparation time
             for (Product item : items) {
                 totalPrep += item.prepTime;
             }
             return totalPrep;
         }
         
-        public void addToOrder(Product newItem, int itemCount) {
+        public void addToOrder(Product newItem, int itemCount) { // add items to the order
             for(int i = 0; i < itemCount; i ++) {
                 items.add(newItem);
             }
@@ -68,9 +68,10 @@ public class OrderSystem {
     
     public static void main(String[] args) {
         
-        ArrayList<Order> orderList = new ArrayList<>();
-        ArrayList<Product> allProducts = new ArrayList<>();
+        ArrayList<Order> orderList = new ArrayList<>(); //list of orders
+        ArrayList<Product> allProducts = new ArrayList<>(); //list of products
         
+        //base products
         Product item1 = new Product("item1", (float)10.5, "Item 1 description", 5);
         allProducts.add(item1);
         Product item2 = new Product("item2", (float)9.0, "Item 2 description", 4);
@@ -84,10 +85,11 @@ public class OrderSystem {
         
         int decider = 0;
         int orderId = 1;
+        //textual command based interface
         while(decider != 3){
             System.out.print("1. New Order \n2. New Product \n3. End Session \n");
             decider = scan.nextInt();
-            
+            //new order
             if(decider == 1){
                 Order newOrder = new Order(orderId);
                 orderId++;
@@ -97,6 +99,7 @@ public class OrderSystem {
                     System.out.print("1. Add Item \n2. Finish Order \n");
                     deciderO = scan.nextInt();
                     
+                    //display products
                     if (deciderO == 1) {
                         int i = 1;
                         for(Product item : allProducts) {
@@ -108,6 +111,7 @@ public class OrderSystem {
                         System.out.print("Enter the Quantity: ");
                         int quantity = scan.nextInt();
                         
+                        //switch case does not account for newly added items
                         switch(nextItem) {
                             case 1: newOrder.addToOrder(item1, quantity);
                                     break;
@@ -120,11 +124,13 @@ public class OrderSystem {
                         }
                     }
                 }
+                //display order
                 orderList.add(newOrder);
                 System.out.print("Order: " + newOrder.id);
                 System.out.print(" || Price: " + newOrder.getPrice());
                 System.out.print(" || ETA: " + newOrder.getTime() + "\n");
             }
+            //add new product
             if(decider == 2){
                 System.out.print("Enter the product name: ");
                 String newName = scan.next();
